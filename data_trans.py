b87e8d6b0e1591c368d8a8bdc18b247ef11919dd
Vladimir Rusch <v.rusch@gmail.com>
2021-11-15 15:39:26 +0100

    n

--- data_trans.py
+++ data_trans.py
@@ -18,11 +18,25 @@ sql = "SELECT date, channel_name, codec, stage, Relapsed, BEelapsed, exit_msg FR
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 
-data = []
-
+#data = []
+dt = {}
 
 for x in myresult:
-    data.append(dict(date = x[0], channel = [x[1]] , codec =[x[2]], stage = [x[3]], Relapsed =[x[4]], exit_msg =[x[5]]))
+    #data.append(dict(channel = str([x[1]]), date = x[0], codec =[x[2]], stage = [x[3]], Relapsed =[x[4]], exit_msg =[x[5]]))
+    dt[str(x[1])] = {}
+    dt[str(x[1])][x[0]] = {}
 
-dx = pd.DataFrame(data)
-print(dx)
+
+
+#dx = pd.DataFrame(data)
+print(dt)
+
+#dictionary = {}
+#dictionary["channel"] = {}
+#dictionary["channel"]["DASH"] = [{"KALT" : 'true'}]
+#dictionary["channel"]["DASH"] = [{"BRPK" : 'true'}]
+#dictionary["channel"]["HLS"] = [{"KALT": 'true'}]
+#dictionary["channel"]["HLS"] = [{"BRPK": 'true'}]
+
+#fig = px.line(dx, x="date", y='Relapsed')
+#fig.show()
