ac293d2bb4f201400cda6f1ef5a5b44581cb0cdf
Vladimir Rusch <v.rusch@gmail.com>
2021-11-16 11:03:02 +0100

    n

--- plot_all.py
+++ plot_all.py
@@ -22,21 +22,11 @@ poc = 0
 for x in myresult:
     poc = poc + 1
     #print(x)
-    dt[poc] = {'channel' : x[1], 'date' : x[0], 'codec' : x[2], 'stage' : x[3], 'Relapsed' : x[4], 'BEelapsed' : x[5]}
-    #data.append(dict(channel = str([x[1]]), date = x[0], codec =[x[2]], stage = [x[3]], Relapsed =[x[4]], exit_msg =[x[5]]))
-  
-  
+    dt[poc] = {'channel' : x[1], 'date' : x[0], 'codec' : x[2], 'stage' : x[3], 'Relapsed' : x[4], 'BEelapsed' : x[5]} 
 
 
 dx = pd.DataFrame(dt)
 print(dx)
-
-#dictionary = {}
-#dictionary["channel"] = {}
-#dictionary["channel"]["DASH"] = [{"KALT" : 'true'}]
-#dictionary["channel"]["DASH"] = [{"BRPK" : 'true'}]
-#dictionary["channel"]["HLS"] = [{"KALT": 'true'}]
-#dictionary["channel"]["HLS"] = [{"BRPK": 'true'}]
 
 #fig = px.line(dx, x=0, y=4)
 #fig.show()
