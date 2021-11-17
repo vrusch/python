02521a1d50178255431b704eecfcbc16816d2b4c
reedkee <v.rusch@gmail.com>
2021-11-17 22:01:08 +0100

    n

---
+++ all_channels_scheduled_upr.py
@@ -0,0 +1,177 @@
+import datetime
+import mysql.connector
+import schedule
+import time
+from KALT_config import *
+import logging
+import logging.handlers as handlers
+import csv
+
+
+#specificke promenne pro test
+partnerID = '3200'
+inputfile = "opc_tlrs.csv"
+
+#DB connect
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="atlantel",
+  database="mydatabase"
+)
+
+#logovani a rotovani logu
+logger = logging.getLogger('my_app')
+logger.setLevel(logging.INFO)
+logHandler = handlers.RotatingFileHandler('./log/channel_availability_test_upr.log', maxBytes=5242880, backupCount=5)
+logHandler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)-0s %(levelname)-0s %(message)s")
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+#ziskani credentials a API hlavicek, phoenixURL
+credentials = user_login(partnerID, 4)
+head = headers(partnerID)
+headerPOST = head[0]
+headerGET = head[1]
+phoenixURL = head[2]
+
+#ziskani user ks
+ks = KALT_ks(apiVersion, partnerID, credentials[0], credentials[1], credentials[2], phoenixURL, headerPOST)
+if ks == 'ERROR':
+    logger.info("No login ks returned")
+else:
+    the_datetime = datetime. datetime. fromtimestamp(ks[3]) 
+    logger.info("[LOGIN]KS EXPIRATION: " + str(the_datetime))
+    logger.info("[LOGIN]BE execution Time: "+str(ks[2]))
+    logger.info("[LOGIN]Elapsed Time: "+str(ks[1]))
+    logger.info("[LOGIN]User KS: " + ks[0])
+
+def func():
+    dateX =  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logger.info("=============================================================================================")
+    logger.info("======= THIST ROUND START: " + dateX + " ===============================================")
+    logger.info("=============================================================================================")
+    #print("ROUND START: " + dateX)
+    #otevrit csv a vrati stream
+    with open(inputfile, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ';')
+        for row in csvreader:
+            channelNumber = row[2]
+            channelName = row[0]
+            assetId = row[1]
+            logger.info("=============================================================================================")
+            logger.info("[TEST]Start test for channel name: " + channelName + " #" + channelNumber + " with ID: " + assetId)
+            r = get_context(assetId, ks[0], headerPOST, phoenixURL)
+            responseDASH = r[0].json()
+            responseHLS = r[1].json()
+            RelapsedDASH = r[2]
+            RelapsedHLS = r[3]
+            RQdate = r[4]
+
+
+    #(assetId, codec, response , headerGET)
+    #analyzeDASH -KALT
+            logger.info("--> Start analyze DASH")
+            DASH_K_BE_execTime = responseDASH['executionTime']
+            try:
+                DASH_K_url = responseDASH['result']['sources'][0]['url']
+                DASH_K_exit_msg = "OK"
+                DASH_K_payload = DASH_K_url
+                logger.info("[RESULT][KALT][DASH]["+channelName+"]["+channelNumber+"]REASON: OK --KALT returned URL")
+                DASH_kalt_reason = True
+            except:
+                Etype = responseDASH['result']['actions'][0]['type']
+                Emsg = responseDASH['result']['messages'][0]['message']
+                DASH_K_exit_msg = "ERROR"
+                DASH_K_payload = "--KALT Error type: " + Etype + "; Error reason: " + Emsg
+                logger.error("[ERROR][KALT][DASH]["+channelName+"]["+channelNumber+"]ERROR: --KALT not returned any URL")
+                logger.error("[ERROR][KALT][DASH]["+channelName+"]["+channelNumber+"]ERROR: type: " + Etype)
+                logger.error("[ERROR][KALT][DASH]["+channelName+"]["+channelNumber+"]ERROR: message: " + Emsg)
+                DASH_kalt_reason = False
+
+            #analyzeDASH -BRPK
+            if DASH_kalt_reason:
+                logger.info("--> CONTINUE TEST FOR DASH -- STAGE BRPR")
+                GETresponse = requests.get(DASH_K_url, headers=headerGET)
+                get_responsecode = GETresponse.status_code
+                if get_responsecode == 200:
+                    DASH_B_elapsed = GETresponse.elapsed.microseconds/1000000
+                    logger.info("[RESULT][BRPK][DASH]["+channelName+"]["+channelNumber+"]Response reason: "+str(GETresponse.reason))
+                    DASH_B_exit_msg = "OK" 
+                    DASH_B_payload = "Reason: "+str(GETresponse.reason)+" Status code: "+str(get_responsecode) 
+                else:
+                    DASH_B_elapsed = GETresponse.elapsed.microseconds/1000000
+                    logger.error("[ERROR][BRPK][DASH]["+channelName+"]["+channelNumber+"]Response reason: "+str(GETresponse.reason))
+                    logger.error("[ERROR][BRPK][DASH]["+channelName+"]["+channelNumber+"]Response status code: "+str(get_responsecode))
+                    logger.error("[ERROR][BRPK][DASH]["+channelName+"]["+channelNumber+"]Elapsed time: "+str(DASH_B_elapsed)) 
+                    DASH_B_exit_msg = "ERROR"
+                    DASH_B_payload = "--BRPK not get Manifest. (wrong URL?) Reason: "+str(GETresponse.reason)+" Status code: "+str(get_responsecode)         
+
+            else:
+                logger.warning("[RESULT][BRPK][DASH]["+channelName+"]["+channelNumber+"]ERROR: --KALT not returned any URL")
+                DASH_B_exit_msgexit_msg = "ERROR"
+                DASH_B_exit_msgpayload = "--KALT not returned any URL"
+                DASH_B_elapse = ""
+
+
+
+    #(assetId, codec, response , headerGET)
+    #analyzeHLS: -KALT
+            logger.info("--> Start analyze HLS")
+            HLS_K_BE_execTime = responseHLS['executionTime']
+            try:
+                HLS_K_url = responseHLS['result']['sources'][0]['url']
+                HLS_K_exit_msg = "OK"
+                HLS_K_payload = HLS_K_url
+                logger.info("[RESULT][KALT][HLS]["+channelName+"]["+channelNumber+"]REASON: OK --KALT returned URL")
+                HLS_kalt_reason = True
+            except:
+                Etype = responseHLS['result']['actions'][0]['type']
+                Emsg = responseHLS['result']['messages'][0]['message']
+                HLS_K_exit_msg = "ERROR"
+                DASH_K_payload = "--KALT Error type: " + Etype + "; Error reason: " + Emsg
+                logger.error("[ERROR][KALT][HLS]["+channelName+"]["+channelNumber+"]ERROR: --KALT not returned any URL")
+                logger.error("[ERROR][KALT][HLS]["+channelName+"]["+channelNumber+"]ERROR: type: " + Etype)
+                logger.error("[ERROR][KALT][HLS]["+channelName+"]["+channelNumber+"]ERROR: message: " + Emsg)
+                HLS_kalt_reason = False
+
+
+            #analyzeHLS -BRPK
+            if HLS_kalt_reason:
+                logger.info("--> CONTINUE TEST FOR HLS -- STAGE BRPR")
+                GETresponse = requests.get(HLS_K_url, headers=headerGET)
+                get_responsecode = GETresponse.status_code
+                if get_responsecode == 200:
+                    HLS_B_elapsed = GETresponse.elapsed.microseconds/1000000
+                    logger.info("[RESULT][BRPK][HLS]["+channelName+"]["+channelNumber+"]Response reason: "+str(GETresponse.reason))
+                    HLS_B_exit_msg = "OK" 
+                    HLS_B_payload = "Reason: "+str(GETresponse.reason)+" Status code: "+str(get_responsecode) 
+                else:
+                    HLS_B_elapsed = GETresponse.elapsed.microseconds/1000000
+                    logger.error("[ERROR][BRPK][HLS]["+channelName+"]["+channelNumber+"]Response reason: "+str(GETresponse.reason))
+                    logger.error("[ERROR][BRPK][HLS]["+channelName+"]["+channelNumber+"]Response status code: "+str(get_responsecode))
+                    logger.error("[ERROR][BRPK][HLS]["+channelName+"]["+channelNumber+"]Elapsed time: "+str(HLS_B_elapsed)) 
+                    HLS_B_exit_msg = "ERROR"
+                    HLS_B_payload = "--BRPK not get Manifest. (wrong URL?) Reason: "+str(GETresponse.reason)+" Status code: "+str(get_responsecode)         
+            else:
+                logger.warning("[RESULT][BRPK][HLS]["+channelName+"]["+channelNumber+"]ERROR: --KALT not returned any URL")
+                HLS_B_exit_msgexit_msg = "ERROR"
+                HLS_B_exit_msgpayload = "--KALT not returned any URL"
+                HLS_B_elapse = ""
+
+            #date, partnerID, channel_num, channel_name, DASH_KALT, DASH_KALT_BE, DASH_BRPK, HLS_KALT, HLS_KALT_BE, HLS_BRPK
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO channel_test_upr (date, partnerID, channel_number, channel_name, DASH_KALT, DASH_KALT_BE, DASH_BRPK, HLS_KALT, HLS_KALT_BE, HLS_BRPK) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            val = (RQdate, partnerID, channelNumber, channelName, RelapsedDASH, DASH_K_BE_execTime, DASH_B_elapsed, RelapsedHLS, HLS_K_BE_execTime, HLS_B_elapsed)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            lastID = mycursor.lastrowid
+            logger.info("1 record inserted, ID:" + str(lastID))
+            RQdate = ""
+
+schedule.every(5).minutes.do(func)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- plot_all.py
+++ plot_all.py
@@ -13,11 +13,48 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-sql = "SELECT date, channel_name, codec, stage, Relapsed, BEelapsed, exit_msg FROM channel_test"
+#sql = "SELECT date, channel_name, codec, stage, Relapsed, BEelapsed, exit_msg FROM channel_test"
+sql = "SELECT date, channel_name, DASH_KALT, DASH_KALT_BE, DASH_BRPK, HLS_KALT, HLS_KALT_BE, HLS_BRPK FROM mydatabase.channel_test_upr"
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 print(len(myresult))
 
+dates = []
+channels = []
+dash_kalt = []
+dash_kalt_be = []
+dash_brpk = []
+hls_kalt = []
+hls_kalt_be = []
+hls_brpk = []
+
+for x in myresult:
+  dates.append(x[0])
+  channels.append(x[1])
+  dash_kalt.append(float(x[2]))
+  dash_kalt_be.append(float(x[3]))
+  dash_brpk.append(float(x[4]))
+  hls_kalt.append(float(x[5]))
+  hls_kalt_be.append(float(x[6]))
+  hls_brpk.append(float(x[7]))
+
+  data = {}
+  data['channels'] = channels
+  data['dates'] = dates
+  data['dash_kalt'] = dash_kalt
+  data['dash_kalt_be'] = dash_kalt_be
+  data['dash_brpk'] = dash_brpk
+  data['hls_kalt'] = hls_kalt
+  data['hls_kalt_be'] = hls_kalt_be
+  data['hls_brpk'] = hls_brpk
+
+dx = pd.DataFrame(data)
+print(dx)
+mask = dx.channels.isin(dx.channels.unique())
+fig = px.line(dx[mask], x="dates", y=["dash_kalt", "dash_kalt_be", "dash_brpk", "hls_kalt", "hls_kalt_be", "hls_brpk"], color='channels', markers=True)
+fig.show()
+
+'''
 date = []
 channel_name = []
 codec = []
@@ -25,7 +62,6 @@ stage = []
 Relapsed = []
 BEelapsed = []
 exit_msg = []
-
 
 for x in myresult:
   date.append(x[0])
@@ -51,14 +87,13 @@ for x in myresult:
   data['BEelapsed'] = BEelapsed
   data['exit_msg'] = exit_msg
 
-
 dx = pd.DataFrame(data)
-print(dx)
+#print(dx)
 #dff = dx[(dx.channel == 'RTS1HD') & (dx.codec == 'DASH') & (dx.stage == 'KALT')]
-#dxa = dx.query("channel in ['RTS1HD'] and codec in ['DASH'] and stage in ['KALT']")
-channels = dx.channel.unique()
-mask = dx.channel.isin(channels)
-fig = px.line(dx[mask], x="date", y="Relapsed", color='channel', markers=True)
 
-#fig = px.line(dxa, x='date', y='Relapsed', markers=True, symbol="stage"
-fig.show()
+dash_kalt = dx.query("codec in ['DASH'] and stage in ['KALT']")
+print(dash_kalt)
+mask = dx.channel.isin(dx.channel.unique())
+fig = px.line(dx[mask], x="date", y=["Relapsed"], color='channel', markers=True)
+#fig.show()
+'''
