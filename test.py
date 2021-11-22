4a6f15dbe80b246472c8b50f59d6414df94d57fb
reedkee <v.rusch@gmail.com>
2021-11-22 23:01:21 +0100

    nn

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 dx = pd.DataFrame(data)
 all_channels = dx.channels.unique()
+dd = dx['dates']
+print(dd)
 
 dates = ['03-02-2021', '03-03-2021', '03-04-2021', '03-05-2021', '03-06-2021', '03-07-2021']
 mytotaldates = {i:datetime.strptime(x, "%m-%d-%Y").date() for i,x in enumerate(dates)}
@@ -19,7 +21,8 @@ a = (list(mytotaldates.keys()))
 print(mytotaldates)
 
 
-app.layout = html.Div(children=[   
+app.layout = html.Div(children=[ 
+    html.H1("Dash Bootstrap Template Demo", className="bg-primary text-white p-2"), 
     dcc.RangeSlider(
         id='Dateslider',
         min=a[0],
@@ -27,7 +30,8 @@ app.layout = html.Div(children=[
         marks=mytotaldates,
         value=[a[0], a[-1]]
     ),
-    html.Div(id='OutputContainer')
+    html.Div(id='OutputContainer'),
+    dcc.Graph(id="line-chart")
 ])
 
 
@@ -38,6 +42,12 @@ app.layout = html.Div(children=[
 def rangerselection(val):
     val = [mytotaldates[val[0]], mytotaldates[val[1]]]
     return f'Selected Date: {val}'
+
+def update_line_chart(): 
+    mask = dx.channels.isin(dx.channels.unique())
+    interpolation = 'linear' # linear, spline, vhv, hvh, vh, hv
+    fig = px.line(dx[mask], x="dates", y=["DASH", "HLS"], color='channels', markers=True, line_shape=interpolation)
+    return fig
 
 
 if __name__ == '__main__':
