9f402e60afaf2665a9972ee7e963c2ad984eb98a
Vladimir Rusch <v.rusch@gmail.com>
2021-11-25 10:01:02 +0100

    mm

--- daemonpy.py
+++ daemonpy.py
@@ -2,22 +2,27 @@ import os, sys
 from daemonize import Daemonize
 import schedule
 import time
+import logging
 
-
+#logging
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, filename="./log/test.log", filemode="a+",
+                        format="%(asctime)-1s %(levelname)-0.5s %(message)s")
+logging.info("hello")
 
 def main():
       # your code here
       def func():
-            print('Alive..')
+            logging.info("hello")
 
-      schedule.every(1).minutes.do(func)
+      schedule.every(10).seconds.do(func)
   
       while True:
             schedule.run_pending()
-            time.sleep(1)
+            #time.sleep(1)
 
 if __name__ == '__main__':
-      myname=os.path.basename(sys.argv[0])
+      myname='TLRS-test_d'
       pidfile='/tmp/%s' % myname       # any name
       daemon = Daemonize(app=myname,pid=pidfile, action=main)
       daemon.start()
--- plot_all.py
+++ plot_all.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 
 #DB connect
 mydb = mysql.connector.connect(
-  host="localhost",
+  host="10.0.0.6",
   user="root",
   password="atlantel",
   database="mydatabase"
@@ -54,16 +54,19 @@ dx['DASH'] = dx.apply(lambda row: row.dash_kalt + row.dash_brpk, axis=1)
 dx['HLS'] = dx.apply(lambda row: row.hls_kalt + row.hls_brpk, axis=1)
 all_channels = dx.channels.unique()
 print(dx.nlargest(5, columns = ['HLS']))
+print()
+print()
 print(dx.nlargest(5, columns = ['DASH']))
 print()
 print()
+print(dx["HLS"].mean())
+print(dx["DASH"].mean())
 
 dt_range = ''
 dt_min = ''
 dt_max = ''
 dt = dx[(dx['dates'] > '2021-11-20 14:00:00') & (dx['dates'] <= '2021-11-20 15:00:00')]
 #print(dt)
-
 #print(dx.nlargest(1, 'DASH'))
 #print(dx.nsmallest(1, 'DASH'))
 #print(dx ["DASH"].mean())
@@ -95,38 +98,3 @@ interpolation = 'linear' # linear, spline, vhv, hvh, vh, hv
 #fig = px.line(dxv, x="dates", y=["DASH", "HLS", "dash_kalt", "dash_kalt_be", "dash_brpk", "hls_kalt", "hls_kalt_be", "hls_brpk"], markers=True, line_shape=interpolation)
 fig = px.line(dx[mask], x="dates", y=["DASH", "HLS"], color='channels', markers=True, line_shape=interpolation)
 fig.show()
-
-'''
-fig = go.Figure()
-fig.add_trace(go.Bar(x=dxv['dates'],
-                y=dxv['DASH'],
-                name='DASH codec',
-                marker_color='rgb(55, 83, 109)'
-                ))
-fig.add_trace(go.Bar(x=dxv['dates'],
-                y=dxv['HLS'],
-                name='HLS codec',
-                marker_color='rgb(26, 118, 255)'
-                ))
-
-fig.update_layout(
-    title='Data for RTS 1 HD channel',
-    xaxis_tickfont_size=14,
-    yaxis=dict(
-        title='Time in ms',
-        titlefont_size=16,
-        tickfont_size=14,
-    ),
-    legend=dict(
-        x=0,
-        y=1.0,
-        bgcolor='rgba(255, 255, 255, 0)',
-        bordercolor='rgba(255, 255, 255, 0)'
-    ),
-    barmode='group',
-    bargap=0.15, # gap between bars of adjacent location coordinates.
-    bargroupgap=0.1 # gap between bars of the same location coordinate.
-)
-fig.update_layout(barmode='group', xaxis_tickangle=-45)
-fig.show()
-'''
